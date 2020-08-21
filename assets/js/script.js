f3a6ccb1a76a7a2a77f3001069fdaef2358d1f7c
vutanguofa <vu.q.tang3@gmail.com>
2020-08-21 16:21:10 -0700

    uploading initial templates for index, css, and js files

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+var questions = [
+    {
+        question: "1. What is the most current Street Figher title?",
+        choices: ["Street Fighter II", "Street Fighter III", "Street Fighter IV", "Street Fighter V"],
+        correctAnswer: 3,
+    },
+    {
+        question: "2. Who does not have the \"Hadouken\" move?",
+        choices: ["Ken", "Ryu", "Guile", "Akuma"],
+        correctAnswer: 2,
+    },
+    {
+        question: "3. Street Fighter was first released in what year?",
+        choices: ["1986", "1987", "1988", "1989"],
+        correctAnswer: 1,
+    },
+    {
+        question: "4. What genre is Street Fighter classified as?",
+        choices: ["Fighting", "Role-playing", "Survival", "Shooter"],
+        correctAnswer: 0,
+    },
+    {
+        question: "5. Street Fighter was developed by: ",
+        choices: ["Sega", "Capcom", "Namco", "Playstation"],
+        correctAnswer: 1,
+    },
+    {
+        question: "6. Who was the creators of Street Fighter?",
+        choices: ["Chunli & Cammie", "Ken & Ryu", "Takashi Nishiyama & Hiroshi Matsumoto", "Balrog & Vega"],
+        correctAnswer: 2,
+    },
+    {
+        question: "7. Who played Guile in the Street Fighter live action game?",
+        choices: ["Bruce Willis", "Brad Pitt", "Tom Cruise", "Jean-Claude Van Damme"],
+        correctAnswer: 3,
+    },
+    {
+        question: "8. Street Fighter V is available on: ?",
+        choices: ["Playstation", "Xbox One", "PC", "All of the above"],
+        correctAnswer: 3,
+    },
+    {
+        question: "9. Who has the Shoryuken as a special move?",
+        choices: ["Ken", "Ryu", "A and B", "None of these characters"],
+        correctAnswer: 2,
+    },
+    {
+        question: "10. Which move will execute with \"Quarter Circle Backwards + Kick / Down – Back – Kick\"?",
+        choices: ["Tatsumaki Senpukyaku", "Hadouken", "Shoryuken", "Super"],
+        correctAnswer: 0,
+    },
+];
+
+
+for (var i = 0; i < questions.length; i++) {
+    // Logs the animal at index position i to the console. This code is executed each we go through the loop.
+    console.log(questions[i]);
+}
+
+/*
+function questionList() {
+    for (var i = 0; i < questions.length; i++) {
+        //console.log(questions[0]);
+        console.log(questions[0])
+    }
+}
+
+questionList();
+*/
