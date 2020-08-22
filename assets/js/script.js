025495b3dc19c0637aeba7c5ee31009df129ed3e
vutanguofa <vu.q.tang3@gmail.com>
2020-08-22 15:31:48 -0700

    uploading js and index updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,264 @@
-// initial score for quiz
-var score = 0;
+(function () {
+  // Functions
+  function buildQuiz() {
+    // variable to store the HTML output
+    const output = [];
 
-// object for questions, choices, and answer
-var questions = [
-    {
-        question: "1. What is the most current Street Figher title?",
-        choices: ["Street Fighter II", "Street Fighter III", "Street Fighter IV", "Street Fighter V"],
-        correctAnswer: 3,
-    },
-    {
-        question: "2. Who does not have the \"Hadouken\" move?",
-        choices: ["Ken", "Ryu", "Guile", "Akuma"],
-        correctAnswer: 2,
-    },
-    {
-        question: "3. Street Fighter was first released in what year?",
-        choices: ["1986", "1987", "1988", "1989"],
-        correctAnswer: 1,
-    },
-    {
-        question: "4. What genre is Street Fighter classified as?",
-        choices: ["Fighting", "Role-playing", "Survival", "Shooter"],
-        correctAnswer: 0,
-    },
-    {
-        question: "5. Street Fighter was developed by: ",
-        choices: ["Sega", "Capcom", "Namco", "Playstation"],
-        correctAnswer: 1,
-    },
-    {
-        question: "6. Who was the creators of Street Fighter?",
-        choices: ["Chunli & Cammie", "Ken & Ryu", "Takashi Nishiyama & Hiroshi Matsumoto", "Balrog & Vega"],
-        correctAnswer: 2,
-    },
-    {
-        question: "7. Who played Guile in the Street Fighter live action game?",
-        choices: ["Bruce Willis", "Jean-Claude Van Damme", "Brad Pitt", "Tom Cruise"],
-        correctAnswer: 1,
-    },
-    {
-        question: "8. Street Fighter V is available on: ?",
-        choices: ["Playstation", "Xbox One", "PC", "All of the above"],
-        correctAnswer: 3,
-    },
-    {
-        question: "9. Who has the Shoryuken as a special move?",
-        choices: ["Ken", "Ryu", "A and B", "None of these characters"],
-        correctAnswer: 2,
-    },
-    {
-        question: "10. Which move will execute with \"Quarter Circle Backwards + Kick / Down – Back – Kick\"?",
-        choices: ["Tatsumaki Senpukyaku", "Hadouken", "Shoryuken", "Super"],
-        correctAnswer: 0,
-    },
-];
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
 
-/*
-for (var i = 0; i < questions.length; i++) {
-    // Logs the animal at index position i to the console. This code is executed each we go through the loop.
-    console.log(questions[i]);
-}
-*/
+        // variable to store the list of possible answers
+        const answers = [];
 
-// function to display questions
-function questionsList() {
-    for (var i = 0; i < questions.length; i++) {
-        // Display current question to user and ask OK/Cancel
-        var displayQuestion = questions[i].question;
-        console.log("Question: " + displayQuestion);
+        // and for each available answer...
+        for (letter in currentQuestion.answers) {
+
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+          );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+          `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+        );
+      }
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults() {
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else {
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if (currentSlide === 0) {
+      previousButton.style.display = 'none';
     }
-};
-
-// function to display choices
-function choicesList() {
-    for (var i = 0; i < questions.length; i++) {
-        // Display current question to user and ask OK/Cancel
-        var displayChoices = questions[i].choices;
-        console.log("Choices: " + displayChoices);
+    else {
+      previousButton.style.display = 'inline-block';
     }
-};
-
-
-// function to display answers
-function answersList() {
-    for (var i = 0; i < questions.length; i++) {
-        // Display current question to user and ask OK/Cancel
-        var correctAnswer = questions[i].correctAnswer;
-        console.log("Correct answer : " + correctAnswer);
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
     }
+    else {
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  // Variables
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: "What is the most current Street Figher title?",
+      answers: {
+        a: "Street Fighter II",
+        b: "Street Fighter III",
+        c: "Street Fighter IV",
+        d: "Street Fighter V"
+      },
+      correctAnswer: "d",
+    },
+    {
+      question: "Who does not have the \"Hadouken\" move?",
+      answers: {
+        a: "Ken",
+        b: "Ryu",
+        c: "Guile",
+        d: "Akuma"
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Street Fighter was first released in what year?",
+      answers: {
+        a: "1986",
+        b: "1987",
+        c: "1988",
+        d: "1989"
+      },
+      correctAnswer: "b",
+    },
+    {
+      question: "Which genre does Street Fighter belong in?",
+      answers: {
+        a: "Fighting",
+        b: "Role-playing",
+        c: "Survival",
+        d: "Shooter"
+      },
+      correctAnswer: "a",
+    },
+    {
+      question: "Street Fighter was developed by: ",
+      answers: {
+        a: "Sega",
+        b: "Capcom",
+        c: "Namco",
+        d: "Playstation"
+      },
+      correctAnswer: "b",
+    },
+    {
+      question: "Who were the creators of Street Fighter?",
+      answers: {
+        a: "Chunli & Cammie",
+        b: "Ken & Ryu",
+        c: "Takashi Nishiyama & Hiroshi Matsumoto",
+        d: "Balrog & Vega"
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Who played Guile in the Street Fighter live action game and movie?",
+      answers: {
+        a: "Bruce Willis",
+        b: "Jean-Claude Van Damme",
+        c: "Brad Pitt",
+        d: "Tom Cruise"
+      },
+      correctAnswer: "b",
+    },
+    {
+      question: "Street Fighter V is available on: ?",
+      answers: {
+        a: "Playstation",
+        b: "Xbox One",
+        c: "PC",
+        d: "All of the above"
+      },
+      correctAnswer: "d",
+    },
+    {
+      question: "Who has the \"Shoryuken\" as a special move?",
+      answers: {
+        a: "Ken",
+        b: "Ryu",
+        c: "A and B",
+        d: "None of these characters"
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Which move will execute with \"quarter Circle Backwards + Kick / Down – Back – Kick\" for Ryu?",
+      answers: {
+        a: "Tatsumaki Senpukyaku",
+        b: "Hadouken",
+        c: "Shoryuken",
+        d: "Super"
+      },
+      correctAnswer: "a",
+    },
+  ];
+
+  // Kick things off
+  // confirm user is ready to start quiz and present question 1
+  do {
+    var startQuiz = confirm("Are you ready to take the quiz?");
+    if (startQuiz === true) {
+      countdown();
+    }
+  } while (startQuiz === false);
+  buildQuiz();
+
+  // Pagination
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  // Show the first slide
+  showSlide(currentSlide);
+
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+})();
+
+
+
+
+// countdown section
+var timerEl = document.getElementById('countdown');
+
+var message =
+  'Game over! You ran out of time';
+
+// Timer that counts down from 5
+function countdown() {
+  var timeLeft = 90;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      alert(message);
+    }
+  }, 1000);
 };
-
-
-questionsList();
-choicesList();
-answersList();
+// end of countdown section
